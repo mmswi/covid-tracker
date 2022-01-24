d3ab038c2566d11d85078d0d5e3fe59ef9b8929a
Mihai Marinescu <marinescu.mihai88@gmail.com>
2022-01-24 15:03:30 +0200

    feat: creates sortable table for the country

--- src/components/Pages/Country/Country.tsx
+++ src/components/Pages/Country/Country.tsx
@@ -1,19 +1,33 @@
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { DataContext } from '../../../App';
+import { getContinentDataByName, getCountryDataByName, mapCountryTableData } from '../../../helpers/map-vaccine-data';
+import { COUNTRY_TABLE_COLUMNS } from '../../../helpers/tableColumns';
+import SortableTable from '../../shared/SortableTable/SortableTable';
+import WithSort from '../../shared/WithSort/WithSort';
 
 import './Country.scss';
 
 function Country() {
   const { continent, country } = useParams();
+  const data = useContext<any>(DataContext);
+  const continentData = getContinentDataByName(data?.groupedByContinent, continent);
+  const countryData = getCountryDataByName(continentData?.values, country);
+  const tableData = mapCountryTableData(countryData);
+  const SortedTable = WithSort(SortableTable, tableData);
+
+  if (!data) {
+    return <div>NOT YET....</div>;
+  }
 
   return (
     <div className="Country">
       <h1>
-        {continent}
-        {' '}
-        and
-        {' '}
         {country}
       </h1>
+      <SortedTable
+        tableColumns={COUNTRY_TABLE_COLUMNS}
+      />
     </div>
   );
 }
