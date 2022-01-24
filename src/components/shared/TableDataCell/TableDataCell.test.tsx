aac0a31059b18d6d45ca00d5486506bc4de9d0e8
Mihai Marinescu <marinescu.mihai88@gmail.com>
2022-01-24 16:22:58 +0200

    fix: deletes table data cell

--- src/components/shared/TableDataCell/TableDataCell.test.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import TableDataCell from './TableDataCell';
-
-it('should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TableDataCell />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
